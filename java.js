af6289d763c89dec1047fc2d4cc1b0a43e0de50f
Ronald Chacon <166889445+DjFunny1@users.noreply.github.com>
2024-07-07 19:34:31 -0400

    Update java.js

--- java.js
+++ java.js
@@ -6,3 +6,21 @@ function loadImage() {
 }
 
 document.getElementById('imageUrlInput').addEventListener('input', loadImage);
+
+//Light and dark theme button
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function toggleDarkMode() {
+    let isDark = document.body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+}
+
+  // On page load
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    }
+});
